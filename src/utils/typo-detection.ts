163abf70f42940270298d64d22690b509e340279
Saqib Ashraf <ashrafsaqib40@gmail.com>
2025-08-14 21:45:31 -0400

    add the typoDector

--- src/utils/typo-detection.ts
+++ src/utils/typo-detection.ts
@@ -1,4 +1,7 @@
+import levenshtein from "fast-levenshtein";
 export function typoDetector(
-  defaultLicense: string,
-  extractedLicense: string,
-) {}
+	defaultLicense: string,
+	extractedLicense: string
+): number {
+	return levenshtein.get(defaultLicense, extractedLicense);
+}
